f9419355b192a5231058fe4ea01ba899c4e93914
stringsplit <77242831+nbitzz@users.noreply.github.com>
2023-11-06 07:02:43 +0000

    well maybe if i

--- index.ts
+++ index.ts
@@ -36,7 +36,7 @@ Bun.serve({
         }
         */
 
-        const neofetch_output = (await exec_promise(isMobile ? "pfetch" : "fastfetch")).stdout
+        const neofetch_output = (await exec_promise(isMobile ? "pfetch" : "fastfetch --load-config neofetch")).stdout
         console.log(neofetch_output)
 
         let res = new Response(
